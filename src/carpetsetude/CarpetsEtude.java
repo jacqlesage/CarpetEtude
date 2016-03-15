0a6e69b5735a3ed06304ca2b6f17f6ff34c82bf3
prabhatgokul <prabhatgokul@gmail.com>
2016-03-15 14:33:07 +1300

    add generateCombinations method

--- src/carpetsetude/CarpetsEtude.java
+++ src/carpetsetude/CarpetsEtude.java
@@ -12,16 +12,22 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
- * @author James
- * @author Ali
- * @author Prabhat
+ * Main method for project: - reads cmd arguments - stores carpet size and flags
+ * - reads input from stdin - stores carpet stock in ArrayList
+ *
+ * @author James Goodman ID: 7482643
+ * @author Prabhat Gokul ID: 5735610
+ * @author Ali Falakah ID: 4818438
  */
 public class CarpetsEtude {
+
     private static int carpetSize;
+    private static int carpetLength;
     private static String flags;
     private static ArrayList<String> carpetStock;
-    private static ArrayList<ArrayList<Integer[]>> normCombinations; 
-    private static ArrayList<ArrayList<Integer[]>> reverseCombinations; 
+    private static ArrayList<ArrayList<CarpetPair>> normCombinations;
+    private static ArrayList<ArrayList<CarpetPair>> reverseCombinations;
+    private static ArrayList<Integer[]> numMatchesList;
 
     /**
      * @param args the command line arguments
@@ -29,7 +35,6 @@ public class CarpetsEtude {
     public static void main(String[] args) {
         carpetSize = 0;
         flags = "";
-        ArrayList<String> carpetStock;
 
         // get cmd arguments - set carpetSize and flags
         if (args[0] != null) {
@@ -38,17 +43,13 @@ public class CarpetsEtude {
         if (args[1] != null) {
             flags = args[1];
         }
-        // test cmd arguments are stored properly
-        System.out.println("carpetSize = " + carpetSize);
-        System.out.println("flags = " + flags);
 
         carpetStock = readInputFile();
-        
-        // test carpetStock is working
-        System.out.println("carpet stock: ");
-        for (String l : carpetStock) {
-            System.out.println(l);
-        }
+        carpetLength = carpetStock.get(0).length();
+        generateCombinations();
+
+        printReadInputTests();
+        printReadCombinationTests();
     }
 
     /**
@@ -57,7 +58,7 @@ public class CarpetsEtude {
      */
     private static ArrayList<String> readInputFile() {
         ArrayList<String> carpetStock = new ArrayList<>();
-        
+
         // temporarily get input from input.txt file
         try {
             FileInputStream is = new FileInputStream(new File("C:\\Users\\admin\\Documents\\Otago uni\\COSC326\\ants\\CarpetEtude\\src\\carpetsetude\\input.txt"));
@@ -66,7 +67,7 @@ public class CarpetsEtude {
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
-        
+
         // use scanner to read each line and add to carpetStock ArrayList
         Scanner in = new Scanner(System.in);
         String line = "";
@@ -74,7 +75,70 @@ public class CarpetsEtude {
             line = in.nextLine();
             carpetStock.add(line);
         }
-        
+
         return carpetStock;
+    }
+
+    private static void generateCombinations() {
+        normCombinations = new ArrayList<>();
+        reverseCombinations = new ArrayList<>();
+        for (int i = 0; i < carpetStock.size() - 1; i++) {
+            ArrayList<CarpetPair> carpetPairList = new ArrayList<>();
+            ArrayList<CarpetPair> reverseCarpetPairList = new ArrayList<>();
+            for (int j = i + 1; j < carpetStock.size(); j++) {
+                int matches = 0, reverseMatches = 0;
+                String line1 = carpetStock.get(i);
+                String line2 = carpetStock.get(j);
+                for (int k = 0; k < carpetLength; k++) {
+                    if (line1.charAt(k) == line2.charAt(k)) {
+                        matches++;
+                    }
+                    if (line1.charAt(carpetLength - 1 - k) == line2.charAt(k)) {
+                        reverseMatches++;
+                    }
+                }
+                CarpetPair carpetPair = new CarpetPair(matches, i, j, false);
+                CarpetPair reverseCarpetPair = new CarpetPair(reverseMatches, i, j, true);
+                carpetPairList.add(carpetPair);
+                reverseCarpetPairList.add(reverseCarpetPair);
+            }
+            normCombinations.add(carpetPairList);
+            reverseCombinations.add(reverseCarpetPairList);
+        }
+    }
+
+    private static void printReadInputTests() {
+        // test cmd arguments are stored properly
+        System.out.println("carpetSize = " + carpetSize);
+        System.out.println("flags = " + flags);
+
+        // test for carpetLength
+        System.out.println("carpet length = " + carpetLength);
+
+        // test carpetStock is working
+        System.out.println("carpet stock: ");
+        for (String l : carpetStock) {
+            System.out.println(l);
+        }
+    }
+
+    private static void printReadCombinationTests() {
+        // test print for normal combinations
+        for (ArrayList list : normCombinations) {
+            for (int i = 0; i < list.size(); i++) {
+                System.out.print(list.get(i).toString());
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+
+        // test print for reverse combinations
+        for (ArrayList list : reverseCombinations) {
+            for (int i = 0; i < list.size(); i++) {
+                System.out.print(list.get(i).toString());
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
     }
 }
